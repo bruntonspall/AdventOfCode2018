da6502acd3180891c134c372a63ad976595f2428
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2018-12-04 00:17:20 +0000

    Solved day 3

---
+++ day3/test_puzzle.py
@@ -0,0 +1,43 @@
+import unittest
+import puzzle
+
+
+class TestBasic(unittest.TestCase):
+
+    def test_parseline(self):
+        self.assertEqual(('#1', {'id': '#1', 'x': 1, 'y': 3, 'width': 4, 'height': 4}), puzzle.parse_line('#1 @ 1,3: 4x4'))
+
+    def test_parse(self):
+        words = """#1 @ 1,3: 4x4
+#2 @ 3,1: 4x4
+#3 @ 5,5: 2x2""".split('\n')
+        data = puzzle.parse(words)
+        self.assertEqual(data['#1'], {'id': '#1', 'x': 1, 'y': 3, 'width': 4, 'height': 4})
+        self.assertEqual(data['#2'], {'id': '#2', 'x': 3, 'y': 1, 'width': 4, 'height': 4})
+        self.assertEqual(data['#3'], {'id': '#3', 'x': 5, 'y': 5, 'width': 2, 'height': 2})
+
+    def test_apply(self):
+        data = [{'id': '#1', 'x': 1, 'y': 3, 'width': 4, 'height': 4}]
+        map = {}
+        self.assertEqual(
+            {(1, 3): ['#1'], (2, 3): ['#1'], (3, 3): ['#1'], (4, 3): ['#1'],
+             (1, 4): ['#1'], (2, 4): ['#1'], (3, 4): ['#1'], (4, 4): ['#1'],
+             (1, 5): ['#1'], (2, 5): ['#1'], (3, 5): ['#1'], (4, 5): ['#1'],
+             (1, 6): ['#1'], (2, 6): ['#1'], (3, 6): ['#1'], (4, 6): ['#1']},
+            puzzle.apply(map, data)
+        )
+
+    def test_solve(self):
+        data = [{'id': '#1', 'x': 1, 'y': 3, 'width': 4, 'height': 4},
+                {'id': '#2', 'x': 3, 'y': 1, 'width': 4, 'height': 4},
+                {'id': '#3', 'x': 5, 'y': 5, 'width': 2, 'height': 2}]
+        self.assertEqual(4, puzzle.solve(data))
+
+    def test_pass(self):
+        data = puzzle.parse(file("input.txt").readlines())
+        answer = puzzle.solve(data.values())
+        self.assertEqual(109785, answer)
+
+
+if __name__ == '__main__':
+    unittest.main()
