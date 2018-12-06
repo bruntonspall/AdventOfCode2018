4270224e4fd993de7c35baef94eabcc866a49596
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2018-12-06 07:10:44 +0000

    Optimise to remove function calls, and use string builtins

--- day5/test_puzzle.py
+++ day5/test_puzzle.py
@@ -3,30 +3,9 @@ import puzzle
 
 
 class TestBasic(unittest.TestCase):
-    def test_has_pair(self):
-        self.assertTrue(puzzle.has_pair("abBA"))
-        self.assertTrue(puzzle.has_pair("aA"))
-        self.assertTrue(puzzle.has_pair("bB"))
-        self.assertTrue(puzzle.has_pair("dabAcCaCBAcCcaDA"))
-        self.assertTrue(puzzle.has_pair("dabAaCBAcCcaDA"))
-        self.assertTrue(puzzle.has_pair("dabCBAcCcaDA"))
-
-        self.assertFalse(puzzle.has_pair("dabCBAcaDA"))
-        self.assertFalse(puzzle.has_pair("abAB"))
-        self.assertFalse(puzzle.has_pair("aabAAB"))
-
-    def test_reduce(self):
-        self.assertEqual("aA", puzzle.do_reduce("abBA"))
-        self.assertEqual("", puzzle.do_reduce("aA"))
-        self.assertEqual("", puzzle.do_reduce("bB"))
-        self.assertEqual("dabAaCBAcCcaDA", puzzle.do_reduce("dabAcCaCBAcCcaDA"))
-        self.assertEqual("dabCBAcCcaDA", puzzle.do_reduce("dabAaCBAcCcaDA"))
-        self.assertEqual("dabCBAcaDA", puzzle.do_reduce("dabCBAcCcaDA"))
-
-        self.assertEqual("abAB", puzzle.do_reduce("abAB"))
-
     def test_solve(self):
         self.assertEqual(10, len(puzzle.solve("dabAcCaCBAcCcaDA")))
+        self.assertEqual(0, len(puzzle.solve("a"*5000+"A"*5000)))
         self.assertEqual(0, len(puzzle.solve("abBA")))
 
 
